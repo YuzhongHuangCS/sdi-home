40751b239354d7132e308c18d3551225537d10db
PillowSky <ihyzi@163.com>
2013-10-12 02:44:41 +0800

    change px to em/%

--- js/main.js
+++ js/main.js
@@ -9,61 +9,67 @@ $(document).ready(function(){
   	var w=window.innerWidth;
   	var h=window.innerHeight;
   	var hslider=h*0.65;
-	$("img.sliderimg").width(w).height(hslider);
-	$("#img2").css({"left":w,"top":"60px"});
-	$("#img3").css({"left":w,"top":"60px"});
-	$("#img4").css({"left":w,"top":"60px"});
-	$("#img5").css({"left":w,"top":"60px"});
-	$("#img6").css({"left":w,"top":"60px"});
+	$(".sliderimg").width(w).height(hslider);
+	$("#img2").css({"left":"100%","top":"4em"});
+	$("#img3").css({"left":"100%","top":"4em"});
+	$("#img4").css({"left":"100%","top":"4em"});
+	$("#img5").css({"left":"100%","top":"4em"});
+	$("#img6").css({"left":"100%","top":"4em"});
+
+
+	//banner animation start
 
 	$("#img1").show(1000);//img1 enter view
 	
 	setTimeout(function(){
-	$("#img1").animate({left:-w},500);
-	$("#img6").hide().css({"left":w,"top":"60px"});
+		$("#img1").animate({left:-w},500);
+		$("#img6").hide().css({"left":w});
 	},5000);//img1 leave view
 	setTimeout(function(){
-		$("#img2").show().animate({left:'0px'},500);
+		$("#img2").show().animate({left:'0'},500);
 	},4800);//img2 enter view
 	setTimeout(function(){
 		$("#img2").animate({left:-w},500);
-		$("#img1").hide().css({"left":w,"top":"60px"});
+		$("#img1").hide().css({"left":w});
 	},8000);//img2 leave view
 	setTimeout(function(){
-		$("#img3").show().animate({left:'0px'},500)
+		$("#img3").show().animate({left:'0'},500)
 	},7800);//img3 enter view
 	setTimeout(function(){
 		$("#img3").animate({left:-w},500)
-		$("#img2").hide().css({"left":w,"top":"60px"});
+		$("#img2").hide().css({"left":w});
 	},11000);//img3 leave view
 	setTimeout(function(){
-		$("#img4").show().animate({left:'0px'},500)
+		$("#img4").show().animate({left:'0'},500)
 	},10800);//img4 enter view
 	setTimeout(function(){
 		$("#img4").animate({left:-w},500)
-		$("#img3").hide().css({"left":w,"top":"60px"});
+		$("#img3").hide().css({"left":w});
 	},14000);//img4 leave view
 	setTimeout(function(){
-		$("#img5").show().animate({left:'0px'},500)
+		$("#img5").show().animate({left:'0'},500)
 	},13800);//img5 enter view
 	setTimeout(function(){
 		$("#img5").animate({left:-w},500);
-		$("#img4").hide().css({"left":w,"top":"60px"});
+		$("#img4").hide().css({"left":w});
 	},17000);//img5 leave view
 	setTimeout(function(){
-		$("#img6").show().animate({left:'0px'},500)
+		$("#img6").show().animate({left:'0'},500)
 	},16800);//img6 enter view
 	setTimeout(function(){
 		$("#img6").animate({left:-w},500);
-		$("#img5").hide().css({"left":w,"top":"60px"});
+		$("#img5").hide().css({"left":w});
 	},20000)//img6 leave view
 	setTimeout(function(){
-		$("#img6").show().animate({left:'0px'},500)
+		$("#img6").show().animate({left:'0'},500)
 	},19800);//img1 enter view
+
+	//watchroom animation start
+
 	$("#watch1").mouseenter(function(){
-  		$("#cover1").show(100).animate({top:'-=5%'},500);
+  		$("#cover1").show().animate({top:'-=24%'},400).delay(50).animate({top:'+=4%'},100);
   	});
-  	$("#watch1").mouseleave(function(){
-  		$("#cover1").animate({top:'+=5%'}.hide(),500);
+  	$("#cover1").mouseleave(function(){
+  		$("#cover1").animate({top:'+=20%'},500).hide(100);
   	});
 });
