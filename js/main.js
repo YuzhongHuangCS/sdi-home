235d9eeb9091ed900fad9d0563ae9dd275c5c95e
PillowSky <ihyzi@163.com>
2013-10-11 23:31:01 +0800

    nightly version

--- js/main.js
+++ js/main.js
@@ -1,14 +1,14 @@
 $(document).ready(function(){
 	$(".nav").mouseenter(function(){
-   		$(".drop").slideDown("slow");
+   		$(".drop").slideDown(900);
   	});
 
 	$(".drop").mouseleave(function(){
-    	$(".drop").slideUp("slow");
+    	$(".drop").slideUp(900);
   	});
   	var w=window.innerWidth;
   	var h=window.innerHeight;
-  	var hslider=h*0.7;
+  	var hslider=h*0.65;
 	$("img.sliderimg").width(w).height(hslider);
 	$("#img2").css({"left":w,"top":"60px"});
 	$("#img3").css({"left":w,"top":"60px"});
@@ -21,44 +21,49 @@ $(document).ready(function(){
 	setTimeout(function(){
 	$("#img1").animate({left:-w},500);
 	$("#img6").hide().css({"left":w,"top":"60px"});
-	},3000);//img1 leave view
+	},5000);//img1 leave view
 	setTimeout(function(){
 		$("#img2").show().animate({left:'0px'},500);
-	},2800);//img2 enter view
+	},4800);//img2 enter view
 	setTimeout(function(){
 		$("#img2").animate({left:-w},500);
 		$("#img1").hide().css({"left":w,"top":"60px"});
-	},6000);//img2 leave view
+	},8000);//img2 leave view
 	setTimeout(function(){
 		$("#img3").show().animate({left:'0px'},500)
-	},5800);//img3 enter view
+	},7800);//img3 enter view
 	setTimeout(function(){
 		$("#img3").animate({left:-w},500)
 		$("#img2").hide().css({"left":w,"top":"60px"});
-	},9000);//img3 leave view
+	},11000);//img3 leave view
 	setTimeout(function(){
 		$("#img4").show().animate({left:'0px'},500)
-	},8800);//img4 enter view
+	},10800);//img4 enter view
 	setTimeout(function(){
 		$("#img4").animate({left:-w},500)
 		$("#img3").hide().css({"left":w,"top":"60px"});
-	},12000);//img4 leave view
+	},14000);//img4 leave view
 	setTimeout(function(){
 		$("#img5").show().animate({left:'0px'},500)
-	},11800);//img5 enter view
+	},13800);//img5 enter view
 	setTimeout(function(){
 		$("#img5").animate({left:-w},500);
 		$("#img4").hide().css({"left":w,"top":"60px"});
-	},15000);//img5 leave view
+	},17000);//img5 leave view
 	setTimeout(function(){
 		$("#img6").show().animate({left:'0px'},500)
-	},14800);//img6 enter view
+	},16800);//img6 enter view
 	setTimeout(function(){
 		$("#img6").animate({left:-w},500);
 		$("#img5").hide().css({"left":w,"top":"60px"});
-	},18000)//img6 leave view
+	},20000)//img6 leave view
 	setTimeout(function(){
 		$("#img6").show().animate({left:'0px'},500)
-	},17800);//img1 enter view
-	
+	},19800);//img1 enter view
+	$("#watch1").mouseenter(function(){
+  		$("#cover1").show(100).animate({top:'-=5%'},500);
+  	});
+  	$("#watch1").mouseleave(function(){
+  		$("#cover1").animate({top:'+=5%'}.hide(),500);
+  	});
 });
