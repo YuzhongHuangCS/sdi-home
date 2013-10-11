b5c573fb50617ee05817ace16057ba7c6aa77f96
PillowSky <ihyzi@163.com>
2013-10-11 16:57:12 +0800

    add middle bar

--- js/main.js
+++ js/main.js
@@ -8,7 +8,7 @@ $(document).ready(function(){
   	});
   	var w=window.innerWidth;
   	var h=window.innerHeight;
-  	var hslider=h*0.6;
+  	var hslider=h*0.7;
 	$("img.sliderimg").width(w).height(hslider);
 	$("#img2").css({"left":w,"top":"60px"});
 	$("#img3").css({"left":w,"top":"60px"});
@@ -17,37 +17,48 @@ $(document).ready(function(){
 	$("#img6").css({"left":w,"top":"60px"});
 
 	$("#img1").show(1000);//img1 enter view
+	
 	setTimeout(function(){
-		$("#img1").animate({left:-w},500);
+	$("#img1").animate({left:-w},500);
+	$("#img6").hide().css({"left":w,"top":"60px"});
 	},3000);//img1 leave view
 	setTimeout(function(){
 		$("#img2").show().animate({left:'0px'},500);
 	},2800);//img2 enter view
 	setTimeout(function(){
 		$("#img2").animate({left:-w},500);
+		$("#img1").hide().css({"left":w,"top":"60px"});
 	},6000);//img2 leave view
 	setTimeout(function(){
 		$("#img3").show().animate({left:'0px'},500)
 	},5800);//img3 enter view
 	setTimeout(function(){
 		$("#img3").animate({left:-w},500)
+		$("#img2").hide().css({"left":w,"top":"60px"});
 	},9000);//img3 leave view
 	setTimeout(function(){
 		$("#img4").show().animate({left:'0px'},500)
 	},8800);//img4 enter view
 	setTimeout(function(){
 		$("#img4").animate({left:-w},500)
+		$("#img3").hide().css({"left":w,"top":"60px"});
 	},12000);//img4 leave view
 	setTimeout(function(){
 		$("#img5").show().animate({left:'0px'},500)
 	},11800);//img5 enter view
 	setTimeout(function(){
-		$("#img5").animate({left:-w},500)
+		$("#img5").animate({left:-w},500);
+		$("#img4").hide().css({"left":w,"top":"60px"});
 	},15000);//img5 leave view
 	setTimeout(function(){
 		$("#img6").show().animate({left:'0px'},500)
-	},14800);//img6 enter viewf
+	},14800);//img6 enter view
 	setTimeout(function(){
-		history.go(0)
-	},18000);
+		$("#img6").animate({left:-w},500);
+		$("#img5").hide().css({"left":w,"top":"60px"});
+	},18000)//img6 leave view
+	setTimeout(function(){
+		$("#img6").show().animate({left:'0px'},500)
+	},17800);//img1 enter view
+	
 });
