e580f6a39eecf2b75532ea88d420547291ee3523
PillowSky <ihyzi@163.com>
2013-10-13 01:04:30 +0800

    depart annimation

--- js/main.js
+++ js/main.js
@@ -80,42 +80,119 @@ $(document).ready(function(){
 	});
 	//watchroom animation start
 
-	$(".merge1").mouseenter(function(){
-  		$(".cover1").show().animate({top:'-=50%'},400).delay(50).animate({top:'+=5%'},100);
+	$(".watchroom .merge1").mouseenter(function(){
+  		$(".watchroom .cover1").show().animate({top:'-=50%'},400).delay(50).animate({top:'+=5%'},100);
   	});
-  	$(".merge1").mouseleave(function(){
-  		$(".cover1").animate({top:'+=45%'},400).hide(100);
+  	$(".watchroom .merge1").mouseleave(function(){
+  		$(".watchroom .cover1").animate({top:'+=45%'},400).hide(100);
+  	});
+  	$(".watchroom .merge2").mouseenter(function(){
+  		$(".watchroom .cover2").slideDown(400);
+  	});
+  	$(".watchroom .merge2").mouseleave(function(){
+  		$(".watchroom .cover2").slideUp(400);
+  	});
+  	$(".watchroom .merge3").mouseenter(function(){
+  		$(".watchroom .cover3").slideDown(400);
+  	});
+  	$(".watchroom .merge3").mouseleave(function(){
+  		$(".watchroom .cover3").slideUp(400);
+  	});
+  	$(".watchroom .merge4").mouseenter(function(){
+  		$(".watchroom .cover4").show(500);
+  	});
+  	$(".watchroom .merge4").mouseleave(function(){
+  		$(".watchroom .cover4").hide(500);
+  	});
+  	$(".watchroom .merge5").mouseenter(function(){
+  		$(".watchroom .cover5").show().animate({left:'10%'},400).delay(50).animate({left:'0%'},100);
+  	});
+  	$(".watchroom .merge5").mouseleave(function(){
+  		$(".watchroom .cover5").animate({left:'-105%'},400).hide(50);
+  	});
+  	$(".watchroom .merge6").mouseenter(function(){
+  		$(".watchroom .cover6").slideDown(400);
+  	});
+  	$(".watchroom .merge6").mouseleave(function(){
+  		$(".watchroom .cover6").slideUp(400);
   	});
 
-  	$(".merge2").mouseenter(function(){
-  		$(".cover2").slideDown(400);
+  	//exhibition animation start
+
+	$(".exhibition .merge1").mouseenter(function(){
+  		$(".exhibition .cover1").show().animate({top:'-=50%'},400).delay(50).animate({top:'+=5%'},100);
   	});
-  	$(".merge2").mouseleave(function(){
-  		$(".cover2").slideUp(400);
+  	$(".exhibition .merge1").mouseleave(function(){
+  		$(".exhibition .cover1").animate({top:'+=45%'},400).hide(100);
+  	});
+  	$(".exhibition .merge2").mouseenter(function(){
+  		$(".exhibition .cover2").slideDown(400);
+  	});
+  	$(".exhibition .merge2").mouseleave(function(){
+  		$(".exhibition .cover2").slideUp(400);
   	});
 
-  	$(".merge3").mouseenter(function(){
-  		$(".cover3").slideDown(400);
+  	$(".exhibition .merge3").mouseenter(function(){
+  		$(".exhibition .cover3").slideDown(400);
   	});
-  	$(".merge3").mouseleave(function(){
-  		$(".cover3").slideUp(400);
+  	$(".exhibition .merge3").mouseleave(function(){
+  		$(".exhibition .cover3").slideUp(400);
   	});
-  	$(".merge4").mouseenter(function(){
-  		$(".cover4").show(500);
+  	$(".exhibition .merge4").mouseenter(function(){
+  		$(".exhibition .cover4").show(500);
   	});
-  	$(".merge4").mouseleave(function(){
-  		$(".cover4").hide(500);
+  	$(".exhibition .merge4").mouseleave(function(){
+  		$(".exhibition .cover4").hide(500);
   	});
-  	$(".merge5").mouseenter(function(){
-  		$(".cover5").show().animate({left:'10%'},400).delay(50).animate({left:'0%'},100);
+  	$(".exhibition .merge5").mouseenter(function(){
+  		$(".exhibition .cover5").show().animate({left:'10%'},400).delay(50).animate({left:'0%'},100);
   	});
-  	$(".merge5").mouseleave(function(){
-  		$(".cover5").animate({left:'-105%'},400).hide(50);
+  	$(".exhibition .merge5").mouseleave(function(){
+  		$(".exhibition .cover5").animate({left:'-105%'},400).hide(50);
   	});
-  	$(".merge6").mouseenter(function(){
-  		$(".cover6").slideDown(400);
+  	$(".exhibition .merge6").mouseenter(function(){
+  		$(".exhibition .cover6").slideDown(400);
   	});
-  	$(".merge6").mouseleave(function(){
-  		$(".cover6").slideUp(400);
+  	$(".exhibition .merge6").mouseleave(function(){
+  		$(".exhibition .cover6").slideUp(400);
+  	});
+
+  	  	//museum annimation start
+
+	$(".museum .merge1").mouseenter(function(){
+  		$(".museum .cover1").show().animate({top:'-=50%'},400).delay(50).animate({top:'+=5%'},100);
+  	});
+  	$(".museum .merge1").mouseleave(function(){
+  		$(".museum .cover1").animate({top:'+=45%'},400).hide(100);
+  	});
+  	$(".museum .merge2").mouseenter(function(){
+  		$(".museum .cover2").slideDown(400);
+  	});
+  	$(".museum .merge2").mouseleave(function(){
+  		$(".museum .cover2").slideUp(400);
+  	});
+  	$(".museum .merge3").mouseenter(function(){
+  		$(".museum .cover3").slideDown(400);
+  	});
+  	$(".museum .merge3").mouseleave(function(){
+  		$(".museum .cover3").slideUp(400);
+  	});
+  	$(".museum .merge4").mouseenter(function(){
+  		$(".museum .cover4").show(500);
+  	});
+  	$(".museum .merge4").mouseleave(function(){
+  		$(".museum .cover4").hide(500);
+  	});
+  	$(".museum .merge5").mouseenter(function(){
+  		$(".museum .cover5").show().animate({left:'10%'},400).delay(50).animate({left:'0%'},100);
+  	});
+  	$(".museum .merge5").mouseleave(function(){
+  		$(".museum .cover5").animate({left:'-105%'},400).hide(50);
+  	});
+  	$(".museum .merge6").mouseenter(function(){
+  		$(".museum .cover6").slideDown(400);
+  	});
+  	$(".museum .merge6").mouseleave(function(){
+  		$(".museum .cover6").slideUp(400);
   	});
 });
